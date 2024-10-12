b52a9dcac478bf41feb0d8cd7b6ec045feb1ae00
Матвей Новиков <originsik@gmail.com>
2024-10-12 23:41:24 +0500

    homework 24

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,6 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
