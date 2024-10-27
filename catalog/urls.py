2983602e563d563b290bfff7ee0e88ad7b7e251b
Матвей Новиков <originsik@gmail.com>
2024-10-27 16:37:36 +0500

    homework 25 FBV and CBV + get_queryset + PEP8

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,9 @@ from catalog.views import (
     BaseTemplateView,
     ContactsTemplateView,
     ProductDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
 )
 
 app_name = CatalogConfig.name
@@ -13,6 +16,8 @@ urlpatterns = [
     path("home/", HomeListView.as_view(), name="home"),
     path("contacts/", ContactsTemplateView.as_view(), name="contacts"),
     path("base/", BaseTemplateView.as_view(), name="base"),
-    path("product/<int:pk>/", ProductDetailView.as_view(),
-         name="product_detail"),
+    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("create/", ProductCreateView.as_view(), name="create_product"),
+    path("<int:pk>/update/", ProductUpdateView.as_view(), name="update_product"),
+    path("<int:pk>/delete/", ProductDeleteView.as_view(), name="delete_product"),
 ]
